db899396a49cd02beaaec5aec94f172fefa14091
a-martynenko1 <a.martynenko1@g.nsu.ru>
2026-05-25 16:11:13 +0700

    Delete StackCalculator directory

--- StackCalculator/src/main/java/calculator/commands/Command.java
+++
@@ -1,8 +0,0 @@
-package calculator.commands;
-
-import calculator.CalculatorException;
-import calculator.Context;
-
-public interface Command {
-    void execute(Context context, String[] args) throws CalculatorException;
-}
